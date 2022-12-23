efd25ddd3fa579608502664b9f0cd08307543fbe
Brandon Kenter <bjkenter@wisc.edu>
2022-12-22 18:30:01 -0600

    Add files via upload

---
+++ Graph Traversal/Shortest Path/Dijkstra's/weightedUndirectedAll.py
@@ -0,0 +1,67 @@
+'''
+Shortest path from a source to every other node in an undirected graph of weights = 1.
+'''
+
+'''
+BFS
+- Distance array 'dist' to store distances to each node
+- Visited set 'vis' to store visited nodes
+- Queue 'q' to store (node, distance to node) pairs
+'''
+class Solution:
+    def shortestPath(self, edges, n, m, src):
+        adj = {i : [] for i in range(n + 1)}
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        dist = [float('inf')] * n
+        vis = set([src])
+        dist[src] = 0
+        q = deque([(src, 0)])
+        
+        while q:
+            for i in range(len(q)):
+                node, node_dist = q.popleft()
+                for nei in adj[node]:
+                    nei_dist = node_dist + 1
+                    if nei not in vis:
+                        vis.add(nei)
+                        q.append((nei, nei_dist))
+                        dist[nei] = nei_dist
+                        
+        for i in range(len(dist)):
+            if dist[i] == float('inf'):
+                dist[i] = -1
+        return dist
+
+'''
+Dijkstra's
+- Distance array 'dist' to store distances to each node
+- Queue 'q' to store (node, distance to node) pairs
+- Doesn't need visited set because "if nei < dist[nei]" acts as a visited check
+'''
+class Solution:
+    def shortestPath(self, edges, n, m, src):
+        adj = {i : [] for i in range(n + 1)}
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        dist = [float('inf')] * n
+        dist[src] = 0
+        q = deque([(src, 0)])
+        
+        while q:
+            for i in range(len(q)):
+                node, node_dist = q.popleft()
+                for nei in adj[node]:
+                    nei_dist = node_dist + 1
+                    if nei_dist < dist[nei]:
+                        q.append((nei, nei_dist))
+                        dist[nei] = nei_dist
+                        
+        for i in range(len(dist)):
+            if dist[i] == float('inf'):
+                dist[i] = -1
+        return dist
