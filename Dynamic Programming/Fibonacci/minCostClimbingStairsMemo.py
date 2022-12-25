8e0b741f7dca3f89345cb590605f85c7dcff05f5
Brandon Kenter <bjkenter@wisc.edu>
2022-12-24 19:15:20 -0600

    Update minCostClimbingStairsMemo.py

--- Dynamic Programming/Fibonacci/minCostClimbingStairsMemo.py
+++ Dynamic Programming/Fibonacci/minCostClimbingStairsMemo.py
@@ -6,10 +6,10 @@ class Solution:
         n = len(cost)
         return min(self.helper(n - 1, cost, memo), self.helper(n - 2, cost, memo))
 
-        def helper(self, i, cost, memo):
-            if i in memo: return memo[i]
+    def helper(self, i, cost, memo):
+        if i in memo: return memo[i]
 
-            left = self.helper(i - 1, cost, memo)
-            right = self.helper(i - 2, cost, memo)
-            memo[i] = cost[i] + min(left, right)
-            return memo[i]
+        left = self.helper(i - 1, cost, memo)
+        right = self.helper(i - 2, cost, memo)
+        memo[i] = cost[i] + min(left, right)
+        return memo[i]
