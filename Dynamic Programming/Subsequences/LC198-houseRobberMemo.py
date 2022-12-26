e5d5d82fe0e6644d01fb1bc92c8956df97258e55
Brandon Kenter <bjkenter@wisc.edu>
2022-12-26 14:32:53 -0600

    memo to dp

--- Dynamic Programming/Subsequences/LC198-houseRobberMemo.py
+++ Dynamic Programming/Subsequences/LC198-houseRobberMemo.py
@@ -3,14 +3,14 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        memo = {}
-        memo[0] = nums[0]
-        memo[1] = max(nums[0], nums[1])
-        return self.rob_helper(len(nums) - 1, memo, nums)
+        dp = {}
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        return self.rob_helper(len(nums) - 1, dp, nums)
         
-    def rob_helper(self, i, memo, nums):
-        if i in memo:
-            return memo[i]
+    def rob_helper(self, i, dp, nums):
+        if i in dp:
+            return dp[i]
         
-        memo[i] = max(nums[i] + self.rob_helper(i - 2, memo, nums), self.rob_helper(i - 1, memo, nums))
-        return memo[i]
+        dp[i] = max(nums[i] + self.rob_helper(i - 2, dp, nums), self.rob_helper(i - 1, dp, nums))
+        return dp[i]
