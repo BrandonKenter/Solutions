37a2e6e3cc2f1396441ed8b164a24cee37c87b7b
Brandon Kenter <bjkenter@wisc.edu>
2022-12-28 12:01:30 -0600

    Update LC62-uniquePathsMemoAndTab.py

--- Dynamic Programming/Matrix/LC62-uniquePathsMemoAndTab.py
+++ Dynamic Programming/Matrix/LC62-uniquePathsMemoAndTab.py
@@ -1,3 +1,6 @@
+'''
+Memoization - Hash map cache and no base case for starting cell (DP seeded)
+'''
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = {}
@@ -15,9 +18,36 @@ class Solution:
             return dp[(i, j)] 
         return helper(m - 1, n - 1)
     
+'''
+Memoization - Matrix cache and base case for starting cell (better imo)
+'''
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[1 for c in range(n)] for r in range(m)]
+        dp = [[-1 for col in range(n)] for row in range(m)]
+
+        def helper(i, j):
+            if dp[i][j] != -1:
+                return dp[i][j]
+            if i == 0 and j == 0:
+                dp[0][0] = 1
+                return dp[0][0]
+            if i < 0 or j < 0:
+                return 0
+            
+            left = helper(i, j - 1)
+            up = helper(i - 1, j)
+            dp[i][j] = left + up
+            return dp[i][j]
+
+        return helper(m - 1, n - 1)
+
+'''
+Tabulation
+'''
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for col in range(n)] for row in range(m)]
+
         for i in range(m):
             for j in range(n):
                 if i == 0 and j == 0:
@@ -26,6 +56,7 @@ class Solution:
                     left = dp[i][j-1] if j > 0 else 0
                     up = dp[i - 1][j] if i > 0 else 0
                     dp[i][j] = left + up
+
         return dp[m-1][n-1]
 
         
