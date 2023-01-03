79aff037cfa9e6842f33f91eec05df616fde6f55
Brandon Kenter <bjkenter@wisc.edu>
2023-01-03 14:14:06 -0600

    Add files via upload

---
+++ Array/LC118-pascalsTriangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+
+        for i in range(numRows - 1):
+            prev = res[-1]
+            new = [1] * (len(prev) + 1)
+            for j in range(1, len(new) - 1):
+                new[j] = prev[j - 1] + prev[j]
+            res.append(new)
+        return res
