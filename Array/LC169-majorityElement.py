4e889a67d46c19ba715ed6560e6a0f25ace2e2c5
Brandon Kenter <bjkenter@wisc.edu>
2023-01-03 13:31:25 -0600

    Add files via upload

---
+++ Array/LC169-majorityElement.py
@@ -0,0 +1,24 @@
+'''
+Update Frequency - O(N) time / O(1) space
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        max_ele = nums[0]
+
+        for num in nums:
+            if count == 0:
+                max_ele = num
+            if num == max_ele: 
+                count += 1
+            else:
+                count -= 1
+        return max_ele
+
+'''
+Hash Map - O(N) time / O(N) space
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        return max(counts, key=counts.get)
