0e4ae2f292eb61acf639c81ed330a76b8b1dff19
Brandon Kenter <bjkenter@wisc.edu>
2023-01-04 23:52:34 -0600

    Add files via upload

---
+++ Binary Search/LC981-timeBasedKey-ValueStore.py
@@ -0,0 +1,30 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.store[key]
+        left, right = 0, len(values) - 1
+        res = ""
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if values[mid][1] == timestamp:
+                return values[mid][0]
+            elif values[mid][1] < timestamp:
+                res = values[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
+                    
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
