1930e272a9f63e99175f473749d6400d074fa031
Brandon Kenter <bjkenter@wisc.edu>
2023-01-05 01:01:21 -0600

    Add files via upload

---
+++ Binary Search/LC367-validPerfectSquare.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 1, num
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square < num:
+                left = mid + 1
+            elif square > num:
+                right = mid - 1
+            else:
+                return True
+        return False
