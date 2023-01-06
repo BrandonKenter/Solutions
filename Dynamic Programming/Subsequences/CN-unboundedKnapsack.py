8e876a959c623660c4fde9ce00dc0c2c9644a20f
Brandon Kenter <bjkenter@wisc.edu>
2023-01-05 22:55:02 -0600

    Add files via upload

--- Dynamic Programming/Subsequences/CN-unboundedKnapsack.py
+++ Dynamic Programming/Subsequences/CN-unboundedKnapsack.py
@@ -1,42 +1,44 @@
 '''
 Memoization
 '''
-def unboundedKnapsack(n, w, profit, weight):
-    dp = [[-1 for col in range(w + 1)] for row in range(n)]
+class Solution: 
+    def unboundedKnapsack(self, weights: List[int], profits: List[int], n: int, maxWeight: int):
+        dp = [[-1 for col in range(maxWeight + 1)] for row in range(n)]
 
-    def helper(i, t):
-        if t < 0:
-            return float('-inf')
-        if i == 0:
-            if weight[0] <= t:
-                return profit[0] * (t // weight[0])
-            else:
-                return 0
-        if dp[i][t] != -1:
-            return dp[i][t]
+        def helper(i, w):
+            if w < 0:
+                return float('-inf')
+            if i == 0:
+                if weights[0] <= w:
+                    return profits[0] * (w // weights[0])
+                else:
+                    return 0
+            if dp[i][w] != -1:
+                return dp[i][w]
 
-        take = profit[i] + helper(i,t-weight[i])
-        not_take = 0 + helper(i-1, t)
-        dp[i][t] = max(take, not_take)
-        return dp[i][t]
-    return helper(n-1, w)
+            take = profits[i] + helper(i, w-weights[i])
+            not_take = 0 + helper(i-1, w)
+            dp[i][w] = max(take, not_take)
+            return dp[i][w]
+        return helper(n-1, maxWeight)
 
 
 '''
 Tabulation
 '''
-def unboundedKnapsack(n, w, profit, weight):
-    dp = [[-1 for col in range(w + 1)] for row in range(n)]
-        
-    for t in range(w + 1):
-        if weight[0] <= t:
-            dp[0][t] = profit[0] * (t // weight[0])
-        else:
-            dp[0][t] = 0
+class Solution:
+    def unboundedKnapsack(self, weights: List[int], profits: List[int], n: int, maxWeight: int):
+        dp = [[-1 for col in range(w + 1)] for row in range(n)]
             
-    for i in range(1, n):
-        for t in range(w + 1):
-            take = profit[i] + dp[i][t-weight[i]] if weight[i] <= t else float('-inf')
-            not_take = 0 + dp[i-1][t]
-            dp[i][t] = max(take, not_take)
-    return dp[n-1][w]
+        for w in range(maxWeight + 1):
+            if weights[0] <= w:
+                dp[0][w] = profits[0] * (w // weights[0])
+            else:
+                dp[0][w] = 0
+                
+        for i in range(1, n):
+            for w in range(maxWeight + 1):
+                take = profits[i] + dp[i][w-weights[i]] if weights[i] <= w else float('-inf')
+                not_take = 0 + dp[i-1][w]
+                dp[i][w] = max(take, not_take)
+        return dp[n-1][maxWeight]
