34c999464a37bcc22cba99c546bcbd134f153db7
Brandon Kenter <bjkenter@wisc.edu>
2023-01-10 19:00:32 -0600

    Add files via upload

---
+++ Dynamic Programming/Fibonacci/LC70-climbingStairs.py
@@ -0,0 +1,28 @@
+'''
+Memoization
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1] * (n+1)
+
+        def helper(i):
+            if i == 0: return 1
+            if i == 1: return 1
+            if dp[i] != -1: return dp[i]
+
+            dp[i] = helper(i-1) + helper(i-2)
+            return dp[i]
+        return helper(n)
+
+
+'''
+Tabulation
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
