dd94faa25d51dca89090198e14d30323aa71ebcc
Brandon Kenter <bjkenter@wisc.edu>
2023-01-10 23:35:08 -0600

    Update LC198-houseRobber.py

--- Dynamic Programming/Subsequences/LC198-houseRobber.py
+++ Dynamic Programming/Subsequences/LC198-houseRobber.py
@@ -25,9 +25,10 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [0] * n
+        
         if n == 1: return nums[0]
-
         dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        
         for i in range(2, n):
             take = nums[i] + dp[i-2]
             skip = 0 + dp[i-1]
