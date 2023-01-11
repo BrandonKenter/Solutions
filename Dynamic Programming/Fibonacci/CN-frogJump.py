220aab6a396cceab8bc3a120326c174d6fcc82cd
Brandon Kenter <bjkenter@wisc.edu>
2023-01-10 21:10:06 -0600

    Add files via upload

---
+++ Dynamic Programming/Fibonacci/CN-frogJump.py
@@ -0,0 +1,30 @@
+'''
+Memoization
+'''
+def frogJump(n: int, heights: List[int]) -> int:
+    dp = [-1] * n
+    
+    def helper(i):
+        if i == 0: return 0
+        if i == 1: return abs(heights[1] - heights[0])
+        if dp[i] != -1: return dp[i]
+    
+        one_jump = abs(heights[i] - heights[i-1]) + helper(i-1)
+        two_jump = abs(heights[i] - heights[i-2]) + helper(i-2)
+        dp[i] = min(one_jump, two_jump)
+        return dp[i]
+    return helper(n-1)
+
+
+'''
+Tabulation
+'''
+def frogJump(n: int, heights: List[int]) -> int:
+    dp = [-1] * n
+    dp[0], dp[1] = 0, abs(heights[1] - heights[0])
+    
+    for i in range(2, n):
+        one_jump = abs(heights[i] - heights[i-1]) + dp[i-1]
+        two_jump = abs(heights[i] - heights[i-2]) + dp[i-2]
+        dp[i] = min(one_jump, two_jump)
+    return dp[n-1]
