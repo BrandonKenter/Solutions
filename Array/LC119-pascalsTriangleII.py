3d2177ddf604b7580ce67fba7dcd59f7f0d26a49
Brandon Kenter <bjkenter@wisc.edu>
2023-01-12 18:49:40 -0600

    Add files via upload

---
+++ Array/LC119-pascalsTriangleII.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        last_row = [1]
+        for i in range(rowIndex):
+            cur_row = [1] * (len(last_row) + 1)
+            for j in range(1, len(cur_row) - 1):
+                cur_row[j] = last_row[j-1] + last_row[j]
+            last_row = cur_row
+        return last_row
