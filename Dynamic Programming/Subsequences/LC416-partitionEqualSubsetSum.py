8f130e3658c608d4cd109bab7ad08ae2dbfeb551
Brandon Kenter <bjkenter@wisc.edu>
2023-01-13 17:56:28 -0600

    Add files via upload

---
+++ Dynamic Programming/Subsequences/LC416-partitionEqualSubsetSum.py
@@ -0,0 +1,44 @@
+'''
+Memoization
+'''
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        nums_sum = sum(nums)
+        if nums_sum % 2: return False
+
+        n, k = len(nums), nums_sum // 2
+        dp = [[None for col in range(k+1)] for row in range(n)]
+
+        def helper(i, k):
+            if k < 0: return False
+            if k == 0: return True
+            if i == 0: return nums[0] == k
+            if dp[i][k] is not None: return dp[i][k]
+
+            take = helper(i-1, k-nums[i])
+            skip = helper(i-1, k)
+            dp[i][k] = take or skip
+            return dp[i][k]
+        return helper(n-1, k)
+
+
+'''
+Tabulation
+'''
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        nums_sum = sum(nums)
+        if nums_sum % 2: return False
+
+        n, k = len(nums), nums_sum // 2
+        dp = [[None for col in range(k+1)] for row in range(n)]
+
+        for i in range(n): dp[i][0] = True
+        for k in range(1, k+1): dp[0][k] = True if nums[0] == k else False
+
+        for i in range(1, n):
+            for k in range(1, k+1):
+                take = dp[i-1][k-nums[i]] if k >= nums[i] else False
+                skip = dp[i-1][k]
+                dp[i][k] = take or skip
+        return dp[n-1][k]
