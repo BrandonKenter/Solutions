3ad881ba72259b94df1136db53fc40e92ac685eb
Brandon Kenter <bjkenter@wisc.edu>
2023-01-14 16:19:42 -0600

    Add files via upload

---
+++ Array/LC2525-shortestDistanceToTargetStringInACircularArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def closetTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        n = len(words)
+        for i in range(n):
+            if words[(startIndex + i) % n] == target:
+                return i
+            elif words[(startIndex - i + n) % n] == target:
+                return i
+        return -1
