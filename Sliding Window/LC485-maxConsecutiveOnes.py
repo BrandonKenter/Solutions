652002c9c0dad0f4be4308815a7f606beb1c915a
Brandon Kenter <bjkenter@wisc.edu>
2023-01-15 13:24:41 -0600

    Update LC485-maxConsecutiveOnes.py

--- Sliding Window/LC485-maxConsecutiveOnes.py
+++ Sliding Window/LC485-maxConsecutiveOnes.py
@@ -1,10 +1,10 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        max_conseq = 0
+        max_consec = 0
         left = 0
         for right in range(len(nums)):
             if nums[right] == 1: 
-                max_conseq = max(max_conseq, right - left + 1)
+                max_consec = max(max_consec, right - left + 1)
             else:
                 left = right + 1
-        return max_conseq
+        return max_consec
