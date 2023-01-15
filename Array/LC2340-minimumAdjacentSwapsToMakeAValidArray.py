ea153b670c6c150b3950b2e62db9a308340d56e1
Brandon Kenter <bjkenter@wisc.edu>
2023-01-14 19:55:30 -0600

    Update LC2340-minimumAdjacentSwapsToMakeAValidArray.py

--- Array/LC2340-minimumAdjacentSwapsToMakeAValidArray.py
+++ Array/LC2340-minimumAdjacentSwapsToMakeAValidArray.py
@@ -1,18 +1,3 @@
-'''
-We have to find the indices of the leftmost instance of 
-the minimum element and the rightmost instance of the maximum
-element. The difference between leftmost and rightmost is 
-why we use < and >= for the min/max val comparisons (we don't 
-want to update min if it is equal to previous min, but we want 
-to update max if it is equal to previous max). The number of 
-swaps is just the distance to the ends of the array. And when 
-the min/max values have to cross each other to get to their 
-respective sides, it takes 1 swap instead of 2 so we decrement 
-swaps by 1 in that case.
-
-Time: O(N)
-Space: O(1)
-'''
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int: 
         min_val, min_i, max_val, max_i = float('inf'), -1, float('-inf'), -1
