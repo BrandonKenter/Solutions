dc304b5b5b8c5b7d3efbb6e3c5d82f5a340b94d8
Brandon Kenter <bjkenter@wisc.edu>
2023-01-15 13:43:59 -0600

    Add files via upload

---
+++ Sliding Window/LC1869-longerContiguousSegmentsOfOnesThanZeros.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkZeroOnes(self, s: str) -> bool:
+
+        def find_longest(val):
+            max_consec, left = 0, 0
+            for right in range(len(s)):
+                if s[right] == val:
+                    max_consec = max(max_consec, right - left + 1)
+                else:
+                    left = right + 1
+            return max_consec
+        
+        max_zero = find_longest('0')
+        max_one = find_longest('1')
+        return max_one > max_zero
