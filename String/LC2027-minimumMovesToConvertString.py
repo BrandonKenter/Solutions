1533e09a683d6fe4ccbf284753234ca21bc4e493
Brandon Kenter <bjkenter@wisc.edu>
2023-01-14 19:20:59 -0600

    Add files via upload

---
+++ String/LC2027-minimumMovesToConvertString.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumMoves(self, s: str) -> int:
+        i = moves = 0
+        while i < len(s):
+            if s[i] == 'X':
+                moves += 1
+                i += 3
+            else:
+                i += 1
+        return moves
