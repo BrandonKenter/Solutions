dfcb677a9a0b7aec17e9149f88cfcb951638381e
Brandon Kenter <bjkenter@wisc.edu>
2023-01-15 13:59:35 -0600

    Add files via upload

---
+++ Sliding Window/LC11784-checkIfBinaryStringHasAtMostOneSegmentOfOnes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkOnesSegment(self, s: str) -> bool:
+        if len(s) == 0:
+            if s[0] == '1': return True
+            else: return False
+
+        segments = 0
+        for i in range(1, len(s)):
+            if s[i] != s[i-1]:
+                segments += 1
+                if segments >= 2: return False
+        return True
