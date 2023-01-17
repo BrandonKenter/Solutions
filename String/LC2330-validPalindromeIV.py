17caee40bd3119b619f765d992940e47c01c38da
Brandon Kenter <bjkenter@wisc.edu>
2023-01-16 20:00:14 -0600

    Add files via upload

---
+++ String/LC2330-validPalindromeIV.py
@@ -0,0 +1,13 @@
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        cnt = 0
+        while left < right:
+            if s[left] != s[right]:
+                cnt += 1
+                if cnt > 2:
+                    return False
+            left += 1
+            right -= 1
+        return True
