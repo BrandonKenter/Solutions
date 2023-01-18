a8bc179f231f892067c9ce4e95a9705b1794b566
Brandon Kenter <bjkenter@wisc.edu>
2023-01-18 02:15:20 -0600

    Add files via upload

---
+++ Hash Map/LC1570-dotProductOfTwoSparseVectors.py
@@ -0,0 +1,19 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.vector = {}
+        for i, num in enumerate(nums):
+            if num != 0:
+                self.vector[i] = num
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        product = 0
+        for i, num in self.vector.items():
+            if i in vec.vector:
+                product += num * vec.vector[i]
+        return product
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
