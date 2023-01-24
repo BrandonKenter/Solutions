d32a4acb5920ec704bc906408e46afa7b806ec09
Brandon Kenter <bjkenter@wisc.edu>
2023-01-24 15:57:40 -0600

    decrease line length

--- Matrix/LC2373-largestLocalValuesInAMatrix.py
+++ Matrix/LC2373-largestLocalValuesInAMatrix.py
@@ -5,7 +5,9 @@ class Solution:
         for r in range(1, n-1):
             row = []
             for c in range(1, n-1):
-                maxi = max(grid[r][c], grid[r+1][c], grid[r+1][c+1], grid[r][c+1], grid[r-1][c+1], grid[r-1][c], grid[r-1][c-1], grid[r][c-1], grid[r+1][c-1])
+                maxi = max(grid[r][c], grid[r+1][c], grid[r+1][c+1], 
+                        grid[r][c+1], grid[r-1][c+1], grid[r-1][c], 
+                        grid[r-1][c-1], grid[r][c-1], grid[r+1][c-1])
                 row.append(maxi)
             res.append(row)
         return res
