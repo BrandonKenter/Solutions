481b0ca589866e7397e540e7b40bd14b88a22432
Brandon Kenter <bjkenter@wisc.edu>
2023-01-26 17:41:35 -0600

    Add files via upload

---
+++ Tree/LC865-smallestSubtreeWithAllTheDeepestNodes.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+        max_depth, res = 0, None
+
+        def dfs(cur, cur_depth):
+            nonlocal max_depth, res
+            if cur is None:
+                return cur_depth
+
+            left = dfs(cur.left, cur_depth + 1)
+            right = dfs(cur.right, cur_depth + 1)
+            max_depth = max(max_depth, left, right)
+            if left == right == max_depth:
+                res = cur
+            return max(left, right)
+        dfs(root, 0)
+        return res
