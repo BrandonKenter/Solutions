da953d028a5246b75d41a9cb03d6d248d04f4c5f
Brandon Kenter <bjkenter@wisc.edu>
2023-01-28 23:50:00 -0600

    Update LC34-findFirstAndLastPositionOfElementInSortedArray.py

--- Binary Search/LC34-findFirstAndLastPositionOfElementInSortedArray.py
+++ Binary Search/LC34-findFirstAndLastPositionOfElementInSortedArray.py
@@ -1,20 +1,24 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [self.find_index(nums, target, True), self.find_index(nums, target, False)]
 
-    def find_index(self, nums, target, is_left):
-        left, right = 0, len(nums) - 1
-        idx = -1
+        def find_position(check_left):
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                m = l + (r - l) // 2
+                val = nums[m]
+                if val == target:
+                    if check_left:
+                        res[0] = m
+                        r = m - 1
+                    else:
+                        res[1] = m
+                        l = m + 1
+                elif val < target:
+                    l = m + 1
+                else:
+                    r = m - 1
 
-        while left <= right:
-            mid = left + (right - left) // 2
-            val = nums[mid]
-            if val == target:
-                idx = mid
-                if is_left: right = mid - 1
-                else: left = mid + 1
-            elif val < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return idx
+        res = [-1, -1]
+        find_position(True)
+        find_position(False)
+        return res
