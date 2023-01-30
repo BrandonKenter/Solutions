15417e13a3749e2f1c3ffec1cc83c6901c881912
Brandon Kenter <bjkenter@wisc.edu>
2023-01-30 15:00:10 -0600

    Add files via upload

---
+++ Array/LC179-largestNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        str_nums = [str(num) for num in nums]
+
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            else:
+                return 1
+        
+        str_nums = sorted(str_nums, key=cmp_to_key(compare))
+        return str(int("".join(str_nums)))
