d542d68a8bf69a20f9f36b2cb11cd570fbb53138
Brandon Kenter <bjkenter@wisc.edu>
2023-02-03 14:55:01 -0600

    Update CN-ninjasTraining.py

--- Dynamic Programming/Matrix/CN-ninjasTraining.py
+++ Dynamic Programming/Matrix/CN-ninjasTraining.py
@@ -2,7 +2,7 @@
 Memoization
 '''
 def ninjaTraining(n: int, points: List[List[int]]) -> int:
-    dp = [[-1 for col in range(4)] for row in range(n)]
+    dp = [[-1 for col in range(3)] for row in range(n)]
     
     def helper(day, last):
         if day == 0:
@@ -21,21 +21,20 @@ def ninjaTraining(n: int, points: List[List[int]]) -> int:
             maxi = max(maxi, p)
         dp[day][last] = maxi
         return dp[day][last]
-    return helper(n-1, 3)
+    return helper(n-1, None)
 
 
 '''
 Tabulation
 '''
 def ninjaTraining(n: int, points: List[List[int]]) -> int:
-    dp = [[-1 for col in range(4)] for row in range(n)]
+    dp = [[-1 for col in range(3)] for row in range(n)]
     dp[0][0] = max(points[0][1], points[0][2])
     dp[0][1] = max(points[0][0], points[0][2])
     dp[0][2] = max(points[0][0], points[0][1])
-    dp[0][3] = max(points[0][0], points[0][1], points[0][2])
     
     for day in range(1, n):
-        for last in range(4):
+        for last in range(3):
             maxi = 0
             for i in range(3):
                 p = 0
@@ -43,4 +42,4 @@ def ninjaTraining(n: int, points: List[List[int]]) -> int:
                     p = points[day][i] + dp[day-1][i]
                 maxi = max(maxi, p)
             dp[day][last] = maxi
-    return dp[n-1][3]
+    return max(dp[n-1])
