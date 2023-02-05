eb33723320ca66ba80ac193eb6dc6e9ce1999ac1
Brandon Kenter <bjkenter@wisc.edu>
2023-02-05 14:31:28 -0600

    Update LC2024-maximizeTheConfusionOfAnExam.py

--- Sliding Window/LC2024-maximizeTheConfusionOfAnExam.py
+++ Sliding Window/LC2024-maximizeTheConfusionOfAnExam.py
@@ -1,3 +1,6 @@
+'''
+Using explicit T/F counter variables
+'''
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         max_consec = t_count = f_count = left = 0
@@ -8,8 +11,25 @@ class Solution:
             to_flip = right - left + 1 - max(t_count, f_count)
             if to_flip <= k:
                 max_consec = max(max_consec, right - left + 1)
-            if to_flip > k:
+            else:
                 if answerKey[left] == 'T': t_count -= 1
                 else: f_count -= 1
                 left += 1
+        return max_consec
+    
+'''
+Using defaultdict
+This implementation is a bit more concise
+'''
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        answer_counts = defaultdict(int)
+        max_consec = left = 0
+        for right in range(len(answerKey)):
+            answer_counts[answerKey[right]] += 1
+            if (right - left + 1) - max(answer_counts.values()) > k:
+                answer_counts[answerKey[left]] -= 1
+                left += 1
+            else:
+                max_consec = max(max_consec, right - left + 1)
         return max_consec
