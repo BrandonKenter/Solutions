694faed5e78a95755b0d44a259965a8442aa1d35
Brandon Kenter <bjkenter@wisc.edu>
2023-02-12 17:48:01 -0600

    Update LC528-randomPickWithWeight.py

--- Binary Search/LC528-randomPickWithWeight.py
+++ Binary Search/LC528-randomPickWithWeight.py
@@ -4,19 +4,17 @@ class Solution:
 
     def __init__(self, w: List[int]):
         self.w = w
-        self.sums = [weight for weight in w]
+        self.pre_sums = [weight for weight in w]
         for i in range(1, len(w)):
-            self.sums[i] += self.sums[i-1]
+            self.pre_sums[i] += self.pre_sums[i-1]
 
     def pickIndex(self) -> int:
-        rand = randint(1, self.sums[-1])
-        left, right = 0, len(self.sums) - 1
+        target = randint(1, self.pre_sums[-1])
+        left, right = 0, len(self.pre_sums) - 1
         while left < right:
             mid = (left + right) // 2
-            s = self.sums[mid]
-            if s == rand:
-                return mid
-            elif s >= rand:
+            mid_sum = self.pre_sums[mid]
+            if mid_sum >= target:
                 right = mid
             else:
                 left = mid + 1
