c1440d80a6da79b97e794e514aef1b1798ef7c5c
Brandon Kenter <bjkenter@wisc.edu>
2023-02-12 01:09:52 -0600

    Add files via upload

---
+++ Binary Search/LC1351-countNegativeNumbersInASortedMatrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        total = 0
+        for arr in grid:
+            l, r = 0, len(arr) - 1
+            l_bound = -1
+            while l <= r:
+                m = (l + r) // 2
+                if arr[m] < 0:
+                    l_bound = m
+                    r = m - 1
+                else:
+                    l = m + 1
+            total += len(arr) - l_bound if l_bound != -1 else 0
+        return total
