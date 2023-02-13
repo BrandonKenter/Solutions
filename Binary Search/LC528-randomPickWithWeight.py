d17ad3358d981725734780d19580c0fc26707d78
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 16:49:22 -0600

    Update LC528-randomPickWithWeight.py

--- Binary Search/LC528-randomPickWithWeight.py
+++ Binary Search/LC528-randomPickWithWeight.py
@@ -1,9 +1,16 @@
 from random import random
 
 # Template 2
-# Find minimal k where condition(k) is True
+# Find minimal k where isCondition(k) is True
+# isCondition is "mid_sum >= target"
+# F F T T T T T <- find first index that is TRUE
+# Why does this meet the traits for Template 2? Because we are
+# searching for the index of the value that is contained in 
+# the pre_sum bucket that corresponds to the target. The leftmost
+# whose prefix sum is >= target is in the bucket/range of values
+# that contains the target. This is where the left pointer will
+# end at, so we can return it.
 class Solution:
-
     def __init__(self, w: List[int]):
         self.w = w
         self.pre_sums = [weight for weight in w]
