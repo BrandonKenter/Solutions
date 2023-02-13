451a8ab7cfe7b66b259d430dde21488bc6afb10a
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 12:37:39 -0600

    Update LC153-findMinimumInRotatedSortedArray.py

--- Binary Search/LC153-findMinimumInRotatedSortedArray.py
+++ Binary Search/LC153-findMinimumInRotatedSortedArray.py
@@ -1,3 +1,19 @@
+# Template 2
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = left + (right-left) // 2
+            print(mid)
+            if nums[mid] <= nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
+
+
+# Template 1
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         min_val = nums[0]
