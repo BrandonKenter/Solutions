325ba7038e5debc52fe5cf22d95129279ee2e1a0
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 16:49:44 -0600

    Update LC35-searchInsertPosition.py

--- Binary Search/LC35-searchInsertPosition.py
+++ Binary Search/LC35-searchInsertPosition.py
@@ -1,5 +1,7 @@
 # Template 2
-# Find minimal k where condition(k) is True
+# Find minimal k where isCondition(k) is True
+# isCondition is "if value at this index >= target"
+# F F T T T T T <- find first index that is TRUE
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)
