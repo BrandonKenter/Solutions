ac63e37f1f8fa84dd7d67299f0008a7fde67a014
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 16:47:00 -0600

    Update LC1351-countNegativeNumbersInASortedMatrix.py

--- Binary Search/LC1351-countNegativeNumbersInASortedMatrix.py
+++ Binary Search/LC1351-countNegativeNumbersInASortedMatrix.py
@@ -1,3 +1,22 @@
+# Template 2
+# Find minimal k where k < 0
+# isCondition is "if arr[m] < 0"
+# F F T T T T T <- find first index that is TRUE
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        total = 0
+        for arr in grid:
+            left, right = 0, len(arr)
+            while left < right:
+                mid = (left + right) // 2
+                if arr[m] < 0:
+                    right = mid
+                else:
+                    left = mid + 1
+            total += len(arr) - left 
+        return total
+
+
 # Template 1
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
@@ -13,22 +32,4 @@ class Solution:
                 else:
                     l = m + 1
             total += len(arr) - l_bound if l_bound != -1 else 0
-        return total
-
-
-
-# Template 2
-# Find minimal k where k < 0
-class Solution:
-    def countNegatives(self, grid: List[List[int]]) -> int:
-        total = 0
-        for arr in grid:
-            l, r = 0, len(arr)
-            while l < r:
-                m = (l + r) // 2
-                if arr[m] < 0:
-                    r = m
-                else:
-                    l = m + 1
-            total += len(arr) - l 
         return total
