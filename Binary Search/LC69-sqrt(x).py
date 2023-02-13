eec28bd43bca28d264db9f4beeaec355a26e7f10
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 00:22:24 -0600

    Update LC69-sqrt(x).py

--- Binary Search/LC69-sqrt(x).py
+++ Binary Search/LC69-sqrt(x).py
@@ -1,3 +1,23 @@
+# Template 2
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        while left < right:
+            mid = left + (right - left) // 2
+            # Finding the minimal k satisfying condition(k) = True
+            # This is either the square root or the square root - 1
+            # because it can be directly equal, or it will be a floating
+            # point of the previous number, so it is rounded down.
+            if mid * mid <= x:
+                left = mid + 1
+            else:
+                right = mid
+        # Since we are ending to the right (last op is left = mid + 1)
+        # we return left - 1.
+        return left - 1
+        
+
+# Template 1
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2: 
