bbd0b5c35974083bb35d95312583e8e2f989bd62
Brandon Kenter <bjkenter@wisc.edu>
2023-02-12 20:57:59 -0600

    Update LC875-kokoEatingBananas.py

--- Binary Search/LC875-kokoEatingBananas.py
+++ Binary Search/LC875-kokoEatingBananas.py
@@ -1,3 +1,21 @@
+# Template 2
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        min_k = len(piles)
+        left, right = 1, max(piles)
+
+        while left < right:
+            cur_k = left + (right - left) // 2
+            cur_h = 0
+            for pile in piles:
+                cur_h += math.ceil(pile / cur_k)
+            if cur_h <= h:
+                right = cur_k
+            else:
+                left = cur_k + 1
+        return left
+
+    
 # Template 1
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
@@ -15,21 +33,3 @@ class Solution:
                 min_k = cur_k
                 right = cur_k - 1
         return min_k
-
-
-# Template 2
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        min_k = len(piles)
-        left, right = 1, max(piles)
-
-        while left < right:
-            cur_k = left + (right - left) // 2
-            cur_h = 0
-            for pile in piles:
-                cur_h += math.ceil(pile / cur_k)
-            if cur_h <= h:
-                right = cur_k
-            else:
-                left = cur_k + 1
-        return left
