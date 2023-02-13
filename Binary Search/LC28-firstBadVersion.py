b94bbcaefa62c2e9aa9d370b9a8582d83c9c7496
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 00:27:57 -0600

    Update LC28-firstBadVersion.py

--- Binary Search/LC28-firstBadVersion.py
+++ Binary Search/LC28-firstBadVersion.py
@@ -1,6 +1,22 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
+# Template 2
+# Find minimal k where isBadVersion(k) is True
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+# Template 1
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 0, n
