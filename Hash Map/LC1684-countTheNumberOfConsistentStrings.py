dbd28778796b905ed9231451e6c8a964585283a0
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 16:56:39 -0600

    Update LC1684-countTheNumberOfConsistentStrings.py

--- Hash Map/LC1684-countTheNumberOfConsistentStrings.py
+++ Hash Map/LC1684-countTheNumberOfConsistentStrings.py
@@ -1,11 +1,12 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         allowed_set = set(allowed)
-        res = []
+        res = 0
         for word in words:
             i = 0
             for char in word:
                 if char in allowed_set: i += 1
                 else: break
-            if i == len(word): res.append(word)
-        return len(res)
+            else:
+                res += 1
+        return res
