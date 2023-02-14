ce562bcfa1c05ba595c5dd9aa0255f414563b761
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 18:57:17 -0600

    Update LC1935-maximumNumberOfWordsYouCanType.py

--- Hash Map/LC1935-maximumNumberOfWordsYouCanType.py
+++ Hash Map/LC1935-maximumNumberOfWordsYouCanType.py
@@ -2,14 +2,11 @@ class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
         broken_set = set(brokenLetters)
         words = text.split(" ")
-
         cnt = 0
         for word in words:
-            i = 0
-            while i < len(word):
-                if word[i] in broken_set:
+            for char in word:
+                if char in broken_set:
                     break
-                i += 1
-            if i == len(word): 
+            else:
                 cnt += 1
         return cnt
