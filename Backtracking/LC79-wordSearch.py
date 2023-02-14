ed5193a046676ecac8dd9a8652894fb1ab38c241
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 21:44:22 -0600

    Update LC79-wordSearch.py

--- Backtracking/LC79-wordSearch.py
+++ Backtracking/LC79-wordSearch.py
@@ -1,38 +1,42 @@
+# Does not have the optimization, so all test cases won't pass
+# This is a interview-friendly solution (optimizations won't be expected in an interview)
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         m, n = len(board), len(board[0])
         vis = set()
 
-        # Edge case handling
-        char_count = defaultdict(int)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                char_count[board[i][j]] += 1
-        for char in word:
-            char_count[char] -= 1
-            if char_count[char] < 0:
-                return False
-
         def backtrack(r, c, i):
-            if i == len(word):
-                return True
+            # Base case to check if state is valid
             if (
                 r not in range(m) or 
                 c not in range(n) or
                 (r, c) in vis or 
                 board[r][c] != word[i]
             ):
-                return False
-
+                return
+            
+            # State is valid, so update state
+            i += 1
             vis.add((r, c))
+
+            # Check if state is a solution and return True if so
+            if i == len(word):
+                return True
+
+            # Make next choices from current state by calling backtrack
+            # for every adjacent cell
             for dr, dc in directions:
                 nei_r, nei_c = dr + r, dc + c
-                if backtrack(nei_r, nei_c, i+1):
+                # Add the if condition to return early since we only care
+                # about finding ONE solution
+                if backtrack(nei_r, nei_c, i):
                     return True
+
+            # Clean up choice 
             vis.remove((r, c))
-            return False
         
+        # Call backtrack on every cell whose char is the first char in word
         for r in range(m):
             for c in range(n):
                 if board[r][c] == word[0] and backtrack(r, c, 0): 
