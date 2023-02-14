360f475b4d0c575ee569267d1ce9ca49936b0232
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 15:26:56 -0600

    Update LC473-matchsticksToSquare.py

--- Backtracking/LC473-matchsticksToSquare.py
+++ Backtracking/LC473-matchsticksToSquare.py
@@ -8,25 +8,31 @@ class Solution:
         # Initialize state collection
         sides = [0] * 4
         
+        # Create backtracking method
+        # State parameters: 
+        #   - i where i is the index of the matchsticks array
         def backtrack(i):
-            # Check if state is a Solution
-            # Return True because asked if ONE solution exists
+            # Check if current state is a solution
+            # If so, return True because asked if ONE solution exists
             if i == len(matchsticks):
                 return True
             
-            # Attempt to make a choice for every possible choice for the
-            # current matchstick
+            # State is valid, so can proceed to make choice on this state
+            # Iterate through choices for current state
             for j in range(4):
-                # Base case check if the state using this choice is valid
-                # If this evaluates to False, we are pruning future unfruitful paths
+                # Check if choice meets constraints before attempting choice
+                # If choice does not meet constraints, continue (prune)
                 if matchsticks[i] + sides[j] <= length:
-                    # State for this choice is valid, so update state
+                    # Choice meets constraints
+                    # Reflect current choice in state collections
                     sides[j] += matchsticks[i]
-                    # Add the if condition to return early since we only 
-                    # care about finding ONE solution
+                    # Recurse on next choice space of next state
+                    # Add if condition and return value
                     if backtrack(i + 1):
                         return True
                     # Clean up choice (backtrack)
+                    # i is automatically cleaned up because we are returning
+                    # to the previous execution context with previous arg
                     sides[j] -= matchsticks[i]
-            return False
+
         return backtrack(0)
