127273bffc0c788b95a51842e6da728bbb282726
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 13:57:02 -0600

    Update LC216-combinationSumIII.py

--- Backtracking/LC216-combinationSumIII.py
+++ Backtracking/LC216-combinationSumIII.py
@@ -1,26 +1,33 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res, combo = [], []
-        used = set()
-        nums = [i for i in range(1, 10)]
+        # Initialize result collecction
+        res = []
+        # Initialize state collection
+        combo = []
 
-        def backtrack(prev, cur_sum):
+        # Create backtracking method
+        # State parameters: 
+        #   - i where i is the number in the range [1, 9]
+        def backtrack(i, cur_sum):
+            # Check if current state is a solution
+            # If so, add to result collection
+            # Return to previous choice space
             if len(combo) == k and cur_sum == n:
                 res.append(combo[:])
                 return
-            if len(combo) == k or cur_sum > n:
+            # Base case to check if current state is valid
+            if len(combo) > k or cur_sum > n:
                 return
             
-            for i in range(prev + 1, 10):
-                if i not in used:
-                    used.add(i)
-                    cur_sum += i
-                    combo.append(i)
-
-                    backtrack(i, cur_sum)
-
-                    used.remove(i)
-                    cur_sum -= i
-                    combo.pop()
-        backtrack(0, 0)
+            # State is valid, so can proceed to make choice on this state
+            # Iterate through choices for current state
+            for j in range(i, 10):
+                # Reflect current choice in state collection
+                combo.append(j)
+                # Recurse on next choice space of next state
+                backtrack(j + 1, cur_sum + j)
+                # Clean up current choice (backtrack)
+                combo.pop()
+                
+        backtrack(1, 0)
         return res
