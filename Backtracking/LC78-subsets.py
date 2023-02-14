36d2b65093c3579c3a68592e34a2bf9047588350
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 12:31:16 -0600

    Update LC78-subsets.py

--- Backtracking/LC78-subsets.py
+++ Backtracking/LC78-subsets.py
@@ -1,24 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res, subset = [], []
+        # Initialize result collecction
+        res = []
+        # Initialize state collection
+        subset = []
         
+        # Create backtrack method
+        # State parameters: 
+        #   - i where i is the index of the nums array
         def backtrack(i):
-            # State is a solution, so add it to solution collection
-            # Return to previous state
+            # Check if state is a solution
+            # If so, add to result collection
             if i == len(nums):
                 res.append(subset[:])
                 return
-            
-            # Make next choice from current state by calling backtrack
-            # on i + 1
+
+            # State is valid, so can proceed to make choice on this state
+            # Reflect current choice in state collection
             subset.append(nums[i])
+            # Recurse on next choice space of next state
             backtrack(i+1)
-            
-            # Clean up decision
+            # Clean up current choice (backtrack)
             subset.pop()
             
-            # Make next choice from current state by calling backtrack
-            # on i + 1 WITHOUT nums[i]
+            # Recurse on choice space of next state
+            # Current choice to NOT include nums[i]
             backtrack(i+1)
         backtrack(0)
         return res
