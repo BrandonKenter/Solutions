3b15a78fdd218ba50eb63667e4e40f16b42ee337
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 13:20:42 -0600

    Update LC39-combinationSum.py

--- Backtracking/LC39-combinationSum.py
+++ Backtracking/LC39-combinationSum.py
@@ -1,19 +1,39 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        out = []
-        combination = []
-        
+        # Initialize result collecction
+        res = []
+        # Initialize state collection
+        combo = []
+
+        # Create backtracking method
+        # State parameters: 
+        #   - i where i is the index of the candidates array
+        #   - cur_sum where cur_sum is the current sum of the picked integers     
         def backtrack(i, cur_sum):
+            # Base case to check if current state is valid
             if i == len(candidates) or cur_sum > target:
-                return 
+                return
+            # Check if current state is a solution
+            # If so, add to result collection
+            # Return to previous choice space
             if cur_sum == target:
-                out.append(combination[:])
+                res.append(combo[:])
                 return
             
-            combination.append(candidates[i])
-            backtrack(i, cur_sum + candidates[i])
-            combination.pop()
+            # State is valid, so can proceed to make choice on this state
+            # Reflect current choice in state collection
+            # Current choice to include candidates[i]
+            combo.append(candidates[i])
+            cur_sum + candidates[i]
+            # Recurse on next choice space of next state
+            backtrack(i, cur_sum)
+            # Clean up current choice (backtrack)
+            combo.pop()
+            cur_sum -= candidates[i]
             
+            # Recurse on choice space of next state
+            # Current choice to NOT include candidates[i]
             backtrack(i + 1, cur_sum)
+            
         backtrack(0, 0)
-        return out
+        return res
