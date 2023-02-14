3eec5c586ddba7edfbe13b8d86754940ad7d86a2
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 16:17:05 -0600

    Update LC93-restoreIPAddresses.py

--- Backtracking/LC93-restoreIPAddresses.py
+++ Backtracking/LC93-restoreIPAddresses.py
@@ -3,36 +3,46 @@ class Solution:
         if len(s) > 12:
             return []
 
-        # Declare result collection
+        # Initialize result collection
         res = []
+        # Initialize state collection
         cur_ip = []
 
-        def backtrack(i, dots):
-            # Check if state is a solution
-            # Add to solution because asked for ALL solutions
-            if dots == 4 and i == len(s):
-                cur_ip[-1] = cur_ip[-1][:-1]
-                res.append("".join(cur_ip))
+        # Create backtracking method
+        # State parameters: 
+        #   - i where i is the index of the string s     
+        def backtrack(i):
+            # Check if current state is a solution
+            # If so, add to result collection
+            # Return to previous choice space
+            if i == len(s) and len(cur_ip) == 4:
+                res.append(".".join(cur_ip))
                 return
 
-            # Base case
-            if dots > 4:
+            # Base case to check if current state is valid
+            if len(cur_ip) > 4:
                 return
             
-            # Attempt to make a choice for every possible choice for the 
-            # current position i in s
+            # State is valid, so can proceed to make choice on this state
+            # Iterate through choices for current state
             for j in range(i, min(i + 3, len(s))):
-                # Base case check if the state using this choice is valid
-                # If this evaluates to False, we are pruning future unfruitful 
-                # paths
+                # Check if choice meets constraints before attempting choice
+                # If choice does not meet constraints, continue (prune)
                 if (
-                    int(s[i:j + 1]) < 256 and
-                    (i == j or s[i] != '0')
+                    int(s[i:j + 1]) >= 256 or
+                    (i != j and s[i] == '0')
                 ):
-                    # State for this choice is valid, so update state
-                    cur_ip.append(s[i:j+1] + '.')
-                    backtrack(j + 1, dots + 1)
-                    # Clean up choice (backtrack)
-                    cur_ip.pop()
-        backtrack(0, 0)
+                    continue
+
+                # Choice meets constraints
+                # Reflect current choice in state collection
+                cur_ip.append(s[i:j+1])
+                # Recurse on next choice space of next state
+                backtrack(j + 1)
+                # Clean up current choice (backtrack)
+                # i is automatically cleaned up because we are returning
+                # to the previous execution context with previous arg
+                cur_ip.pop()
+
+        backtrack(0)
         return res
