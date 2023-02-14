aad935649aff03a275bd2cc5602b4b787d8db9d7
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 19:21:44 -0600

    Update LC2215-findTheDifferenceOfTwoArrays.py

--- Hash Map/LC2215-findTheDifferenceOfTwoArrays.py
+++ Hash Map/LC2215-findTheDifferenceOfTwoArrays.py
@@ -1,13 +1,13 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         nums1_set, nums2_set = set(nums1), set(nums2)
-        dist_1, dist_2 = set(), set()
+        dist_1, dist_2 = [], []
         
-        for num in nums1:
+        for num in nums1_set:
             if num not in nums2_set:
-                dist_1.add(num)
-        for num in nums2:
+                dist_1.append(num)
+        for num in nums2_set:
             if num not in nums1_set:
-                dist_2.add(num)
+                dist_2.append(num)
         
-        return [list(dist_1), list(dist_2)]
+        return [dist_1, dist_2]
