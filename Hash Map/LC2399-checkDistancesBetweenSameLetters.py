c3efa0d55212ab64e4c96eac4d37bb496bc1f76e
Brandon Kenter <bjkenter@wisc.edu>
2023-02-13 19:04:00 -0600

    Update LC2399-checkDistancesBetweenSameLetters.py

--- Hash Map/LC2399-checkDistancesBetweenSameLetters.py
+++ Hash Map/LC2399-checkDistancesBetweenSameLetters.py
@@ -1,14 +1,11 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
-        letter_indices = defaultdict(list)
+        seen = defaultdict(int)
         for i, char in enumerate(s):
-            letter_indices[char].append(i)
-        
-        for i in range(26):
-            char = chr(i + ord('a'))
-            if char in letter_indices:
-                index_1, index_2 = letter_indices[char]
-                if index_2 - index_1 - 1 != distance[i]:
-                    print(i)
+            if char in seen:
+                idx = ord(char) - ord('a')
+                if distance[idx] != i - seen[char] - 1:
                     return False
+            else:
+                seen[char] = i
         return True
