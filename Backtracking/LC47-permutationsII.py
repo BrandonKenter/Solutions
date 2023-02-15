039e3c20850a6c925bfa6f4092d0e6697000f9f3
Brandon Kenter <bjkenter@wisc.edu>
2023-02-14 20:45:31 -0600

    Add files via upload

---
+++ Backtracking/LC47-permutationsII.py
@@ -0,0 +1,38 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        # Initialize result collection
+        res = []
+        # Initialize state collection
+        perm = []
+        used = [False] * len(nums)
+        nums.sort()  # sort the input array
+        
+        # Create backtracking method
+        def backtrack():
+            # Check if current state is a solution
+            # If so, add to result collection
+            # Return to previous choice space
+            if len(perm) == len(nums):
+                res.append(perm[:])
+                return
+            
+            # State is valid, so can proceed to make choice on this state
+            # Iterate through choices for current state
+            for i in range(len(nums)):
+                # Check if choice meets constraints before attempting choice
+                # If choice does not meet constraints OR we skipped this number
+                # on the previous choice, continue (prune)
+                if used[i] or (i > 0 and nums[i] == nums[i-1] and not used[i-1]):
+                    continue
+                # Choice meets constraints
+                # Reflect current choice in state collection
+                used[i] = True
+                perm.append(nums[i])
+                # Recurse on next choice space of next state
+                backtrack()
+                # Clean up current choice (backtrack)
+                used[i] = False
+                perm.pop()
+
+        backtrack()
+        return res
