b22d58c26cc25581203d346f346c08ae7080dcb0
Brandon Kenter <bjkenter@wisc.edu>
2023-03-03 17:33:38 -0600

    Add files via upload

---
+++ String/LC2259-removeDigitFromNumberToMaximizeResult.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        count = 0
+        for d in number:
+            if d == digit: count += 1
+
+        cur_count = 0
+        for i in range(len(number)):
+            if number[i] == digit:
+                cur_count += 1
+                if i == len(number) - 1:
+                    return number[:-1]
+                elif cur_count == count:
+                    return number[:i] + number[i+1:]
+                elif int(number[i+1]) > int(number[i]):
+                    return number[:i] + number[i+1:]
