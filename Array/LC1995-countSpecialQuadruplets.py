094b93ad1a94d7b6f2d96e4d001a8129c78e6213
Brandon Kenter <bjkenter@wisc.edu>
2023-03-05 16:09:06 -0600

    Add files via upload

---
+++ Array/LC1995-countSpecialQuadruplets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        cnt = 0
+        for i in range(len(nums) - 3):
+            for j in range(i + 1, len(nums) - 2):
+                for k in range(j + 1, len(nums) - 1):
+                    for l in range(k + 1, len(nums)):
+                        if nums[i] + nums[j] + nums[k] == nums[l]:
+                            cnt += 1
+        return cnt
