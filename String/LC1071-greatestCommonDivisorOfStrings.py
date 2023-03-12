6dd3d58ae7b31a737eb21e1b329606d3a25b0547
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-12 03:38:44 -0500

    change hash map to hash set

--- String/LC1071-greatestCommonDivisorOfStrings.py
+++ String/LC1071-greatestCommonDivisorOfStrings.py
@@ -1,25 +1,23 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         m, n = len(str1), len(str2)
-        div_len = defaultdict(int)
-        maxi = 0
-        maxi_str = ""
+        divs = set()
+        maxi, maxi_str = 0, ""
         
         for i in range(len(str1)):
             t = i + 1
             if m % t != 0:
                 continue
             if str1[:i+1] * (m // t) == str1:
-                div_len[tuple(str1[:i+1])] = i + 1
+                divs.add(tuple(str1[:i+1]))
         
         for i in range(len(str2)):
             t = i + 1
             if n % t != 0:
                 continue
             if str2[:i+1] * (n // t) == str2:
-                if tuple(str2[:i+1]) in div_len:
+                if tuple(str2[:i+1]) in divs:
                     if i + 1 > maxi:
                         maxi = i + 1
                         maxi_str = str2[:i+1]
         return maxi_str
-        
