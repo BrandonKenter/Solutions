8dad9c4ac8b7c3ed203ae743b04c92ece8e9e46c
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-12 00:17:08 -0600

    new

---
+++ Math/LC1271-hexspeak.py
@@ -0,0 +1,20 @@
+class Solution:
+    def toHexspeak(self, num: str) -> str:
+        letters = {'A', 'B', 'C', 'D', 'E', 'F'}
+        num = hex(int(num))[2:]
+        res = []
+        for c in num:
+            if c.isnumeric():
+                if c != '0' and c != '1':
+                    return "ERROR"
+                elif c == '0':
+                    res.append('O')
+                else:
+                    res.append('I')
+            else:
+                if c.upper() not in letters:
+
+                    return "ERROR"
+                else:
+                    res.append(c.upper())
+        return "".join(res)
