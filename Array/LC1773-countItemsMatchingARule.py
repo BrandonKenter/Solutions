0ba3e8f46efe841b91c1b26a172a5e706f7b572c
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-17 20:25:47 -0500

    new

---
+++ Array/LC1773-countItemsMatchingARule.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        count = 0
+        for t, c, n in items:
+            if ruleKey == "type" and ruleValue == t:
+                count += 1
+            elif ruleKey == "color" and ruleValue == c:
+                count += 1
+            elif ruleKey == "name" and ruleValue == n:
+                count += 1
+        return count
+        
