ac189a67720c7d45d71728d01310b940b783e044
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-18 21:55:17 -0500

    updated

--- Matrix/LC994-rottingOranges.py
+++ Matrix/LC994-rottingOranges.py
@@ -1,36 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        M, N = len(grid), len(grid[0])
-        v = set()
-        q = deque() 
+        m, n = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        q = deque()
+        vis = set()
         fresh = 0
-        
-        for r in range(M):
-            for c in range(N):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 2:
                     q.append((r, c))
-                    v.add((r, c))
+                    vis.add((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
         
-        t = 0
-        while q and fresh > 0:
+        time = 0
+        while q and fresh:
             for i in range(len(q)):
                 r, c = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-                
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
+                    nei_r, nei_c = dr + r, dc + c
                     if (
-                        row not in range(M) or 
-                        col not in range(N) or 
-                        (row, col) in v or
-                        grid[row][col] != 1
+                        nei_r in range(m) and 
+                        nei_c in range(n) and 
+                        (nei_r, nei_c) not in vis and 
+                        grid[nei_r][nei_c] == 1
                     ):
-                        continue
-                    
-                    q.append((row, col))
-                    v.add((row, col))
-                    fresh -= 1
-            t += 1
-        return t if fresh == 0 else -1
+                        vis.add((nei_r, nei_c))
+                        fresh -= 1
+                        q.append((nei_r, nei_c))
+            time += 1
+        return time if fresh == 0 else -1
+    
