37a0b502f35409596fbee6c92193b61e478461eb
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-19 18:13:42 -0500

    update and rename

--- Graph/GFG-topologicalSort.py
+++ Graph/GFG-topologicalSort.py
@@ -8,41 +8,42 @@ class Solution:
         
         def dfs(cur):
             if cur in vis:
-                return 
-            
-            for nei in adj[cur]:
-                dfs(nei)
+                return
             
             vis.add(cur)
+            for nei in adj[cur]:
+                dfs(nei)
             res.append(cur)
         
-        for i in range(V):
-            if i not in vis:
-                dfs(i)
+        for node in range(V):
+            if node not in vis:
+                dfs(node)
         return res[::-1]
+
 
 '''
 BFS (Kahn's)
 '''
 class Solution:
     def topoSort(self, V, adj):
-        indegree = [0] * V
-        for a in range(V):
-            for b in adj[a]:
-                indegree[b] += 1
+        indegree = [0 for i in range(V)]
+        for u in range(V):
+            for v in adj[u]:
+                indegree[v] += 1
         
         q = deque()
-        for i in range(V):
-            if indegree[i] == 0:
-                q.append(i)
+        for node in range(V):
+            if indegree[node] == 0:
+                q.append(node)
         
-        ans = []
+        res = []
         while q:
             cur = q.popleft()
-            ans.append(cur)
+            res.append(cur)
             
             for nei in adj[cur]:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-        return ans
+        return res
+    
