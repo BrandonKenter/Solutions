26522ffd8351382a422a10bcb163f8fbb0f09a18
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-18 20:47:59 -0500

    updated

--- Matrix/LC733-floodFill.py
+++ Matrix/LC733-floodFill.py
@@ -1,24 +1,57 @@
+'''
+DFS
+O(M*N) time / O(M*N) space
+'''
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        M, N = len(image), len(image[0])
-        v = set()
-        old = image[sr][sc]
-        
+        m, n = len(image), len(image[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        s_color = image[sr][sc]
+        vis = set()
+
         def dfs(r, c):
-            if (
-                r not in range(M) or 
-                c not in range(N) or 
-                (r, c) in v or 
-                image[r][c] != old
-            ):
-                return
-            
-            v.add((r, c))
+            vis.add((r, c))
             image[r][c] = color
-            
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
             for dr, dc in directions:
-                row, col = dr + r, dc + c
-                dfs(row, col)
+                nei_r, nei_c = dr + r, dc + c
+                if (
+                    nei_r in range(m) and 
+                    nei_c in range(n) and 
+                    (nei_r, nei_c) not in vis and 
+                    image[nei_r][nei_c] == s_color
+                ):
+                    dfs(nei_r, nei_c)
+        
         dfs(sr, sc)
+        return image
+
+
+
+'''
+BFS
+O(M*N) time / O(M*N) space
+'''
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m, n = len(image), len(image[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        s_color = image[sr][sc]
+        vis = set([(sr, sc)])
+        q = deque([(sr, sc)])
+
+        while q:
+            r, c = q.popleft()
+            image[r][c] = color
+            for dr, dc in directions:
+                nei_r, nei_c = dr + r, dc + c
+                if (
+                    nei_r in range(m) and 
+                    nei_c in range(n) and 
+                    (nei_r, nei_c) not in vis and
+                    image[nei_r][nei_c] == s_color
+                ):
+                    vis.add((nei_r, nei_c))
+                    q.append((nei_r, nei_c))
+                    image[nei_r][nei_c] = color
+
         return image
