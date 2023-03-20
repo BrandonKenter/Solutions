db01cb41b721df377ce52b43d8edafe248a9126e
Brandon Kenter <kenterbrandon@gmail.com>
2023-03-20 18:00:10 -0500

    added comments

--- Array/LC1909-removeOneElementToMakeTheArrayStrictlyIncreasing.py
+++ Array/LC1909-removeOneElementToMakeTheArrayStrictlyIncreasing.py
@@ -4,7 +4,9 @@ class Solution:
         for i in range(1, len(nums)):
             if nums[i-1] >= nums[i]:
                 count += 1
+                # Remove current 
                 if i > 1 and nums[i-2] >= nums[i]:
                     nums[i] = nums[i-1]
+                # Remove previous (do nothing)
         return count < 2
         
