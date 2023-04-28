c2fde9f77d458d66dfd48a868378bba29dab99b3
Brandon Kenter <bjkenter@wisc.edu>
2023-04-28 16:09:13 -0500

    updated to reflect template

--- Backtracking/LC332-reconstructItinerary.py
+++ Backtracking/LC332-reconstructItinerary.py
@@ -4,35 +4,40 @@ class Solution:
         
         # Initialize state collection
         res = ['JFK']
+        counts = defaultdict(int)
         adj = defaultdict(list)
         for depart, dest in tickets:
             adj[depart].append(dest)
+            counts[(depart, dest)] += 1
+        
+        vis = defaultdict(int)
 
         # Create backtracking method
         # State parameters: 
         #   - cur where cur is the current airport
-        def backtrack(cur):
+        def backtrack(cur, count):
             # Check if current state is a solution
             # If so, return True because asked if ONE solution exists
-            if len(res) == len(tickets) + 1:
+            if count == len(tickets):
                 return True
 
             # State is valid, so can proceed to make choice on this state
             # Iterate through choices for current state
-            temp = adj[cur]
-            for i, nei in enumerate(temp):
+            for nei in adj[cur]:
                 # Reflect current choice in state collections
-                res.append(nei)
-                adj[cur].pop(i)
-                # Recurse on next choice space of next state
-                # Add if condition and return value
-                if backtrack(nei): 
-                    return True
-                # Clean up choice (backtrack)
-                # cur is automatically cleaned up because we are returning
-                # to the previous execution context with previous arg
-                res.pop()
-                adj[cur].insert(i, nei)
+                if vis[(cur, nei)] != counts[(cur, nei)]:
+                    res.append(nei)
+                    vis[(cur, nei)] += 1
+                    # Recurse on next choice space of next state
+                    # Add if condition and return value
+                    if backtrack(nei, count + 1): 
+                        return True
+                    # Clean up choice (backtrack)
+                    # cur is automatically cleaned up because we are returning
+                    # to the previous execution context with previous arg
+                    res.pop()
+                    vis[(cur, nei)] -= 1
   
-        backtrack('JFK')
+        backtrack('JFK', 0)
         return res
+        
