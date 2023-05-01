6c81bfaa920389d8c6423887d910160461a95ead
Brandon Kenter <kenterbrandon@gmail.com>
2023-05-01 15:49:52 -0500

    updated to be more concise

--- String/LC791-customSortString.py
+++ String/LC791-customSortString.py
@@ -2,23 +2,15 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         indices = defaultdict(int)
         for i, c in enumerate(order):
-            if c not in indices:
-                indices[c] = i
+            indices[i] = c
         
-        # Use array buckets because empty string buckets 
-        # with concatenation will be O(N^2) time complexity
-        buckets = [[] for i in range(201)]
-        for c in s:
-            # Put the character in the bucket for its respective index in indices
-            # If c is not in the indices map, it defaults to 0
-            # This is fine because these chars can be anywhere in the result string
-            # So the chars in s that are not in order will be at the beginning of the res string
-            i = indices[c] 
-            buckets[i].append(c)
-        
+        s_counts = Counter(s)
         res = []
-        for c_arr in buckets:
-            if c_arr:
-                res.append("".join(c_arr))
+        for _, char in sorted(indices.items()):
+            if char in s_counts:
+                res.append(char * s_counts[char])
+                s_counts.pop(char)
+        for char, count in s_counts.items():
+            res.append(count * char)
         return "".join(res)
         
