39f6973c412a07fdd91bc41b3ca83c5c5d2f3835
Brandon Kenter <bjkenter@wisc.edu>
2023-05-03 02:14:49 -0500

    Update LC1446-consecutiveCharacters.py

--- Sliding Window/LC1446-consecutiveCharacters.py
+++ Sliding Window/LC1446-consecutiveCharacters.py
@@ -1,11 +1,9 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        max_consec, cur_char, left = 1, s[0], 0
+        max_consec, left = 1, 0
     
         for right in range(len(s)):
-            if s[right] == cur_char:
-                max_consec = max(max_consec, right - left + 1)
-            else:
-                left = right
-                cur_char = s[left]
+            while s[left] != s[right]:
+                left += 1
+            max_consec = max(max_consec, right - left + 1)
         return max_consec
