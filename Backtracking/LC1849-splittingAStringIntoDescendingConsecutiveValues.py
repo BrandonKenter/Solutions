2e98eec4682cfd92bf406ac4d77f33f310f46059
Brandon Kenter <kenterbrandon@gmail.com>
2023-05-11 01:01:58 -0500

    cleaner solution

--- Backtracking/LC1849-splittingAStringIntoDescendingConsecutiveValues.py
+++ Backtracking/LC1849-splittingAStringIntoDescendingConsecutiveValues.py
@@ -1,36 +1,20 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-        # Create backtracking method
-        def backtrack(i, prev, count):
-            # Check if current state is a solution
-            # If so, return True because asked if ONE solution exists
-            if i == len(s) and count >= 2:
+        values = []
+
+        def backtrack(i):
+            if i == len(s) and len(values) >= 2:
                 return True
+            elif i == len(s):
+                return False
             
-            # State is valid, so can proceed to make choice on this state
-            # Iterate through choices for current state
             for j in range(i, len(s)):
-                # Check if choice meets constraints before attempting choice
-                # If choice does not meet constraints, continue (prune)
-                nxt = int(s[i:j+1])
-                if prev is not None and nxt != prev - 1:
-                    continue
-                # Choice meets constraints
-                # Reflect current choice in state collections
-                # In this case, there are no state collections to update
-                #   since the state is just int variables as arguments. If
-                #   we had a need for a set or array that tracked the 
-                #   state, we would update it here.
-                # Recurse on next choice space of next state
-                # Add if condition and return value
-                if backtrack(j + 1, nxt, count + 1):
-                    return True
-                # Clean up choice (backtrack)
-                # There is nothing to clean up because the arguments
-                #   are the only way we track the state. So when we
-                #   backtrack, the execution context uses the arguments
-                #   in that call. If we use state collections outside of
-                #   the parameter list, this is where we would undo 
-                #   the change to that collection for this choice.
+                sub = s[i:j+1]
+                if len(values) == 0 or int(sub) == values[-1] - 1:
+                    values.append(int(sub))
+                    if backtrack(j+1):
+                        return True
+                    values.pop()
             return False
-        return backtrack(0, None, 0)
+        return backtrack(0)
+        
