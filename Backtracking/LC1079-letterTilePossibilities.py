e3542233af774eb1c452ca84aa7c62e1775a66ae
Brandon Kenter <kenterbrandon@gmail.com>
2023-08-18 14:37:13 -0500

    Update LC1079-letterTilePossibilities.py

--- Backtracking/LC1079-letterTilePossibilities.py
+++ Backtracking/LC1079-letterTilePossibilities.py
@@ -1,10 +1,11 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         # Initialize result collection
-        res = set()
+        count = 0
         # Initialize state collection
-        used = [False] * len(tiles)
         perm = []
+        used = [False for i in range(len(tiles))]
+        tiles = sorted(tiles)
 
         # Create backtracking method
         # State parameters: 
@@ -12,26 +13,28 @@ class Solution:
         def backtrack():
             # Check if we reached end of state space tree
             # Don't add to result collection here because we are 
-            #   adding all non-empty sequences to the collection 
-            #   when we make our choice to add to the sequence
+            #   adding to non-empty sequence count when we make
+            #    our choice to add to the sequence
             # Return to previous choice space
+            nonlocal count
             if len(perm) == len(tiles):
                 return
             
             # State is valid, so can proceed to make choice on this state
             # Iterate through choices for current state
-            for i in range(len(tiles)):
+            for i in range(len(used)):
                 # Check if choice meets constraints before attempting choice
                 # If choice does not meet constraints, continue (prune)
-                if used[i]:
-                    continue
+                if used[i] == False:
+                    if i > 0 and used[i - 1] == False and tiles[i] == tiles[i-1]:
+                        continue
                 
                 # Choice meets constraints
-                # Choice is to use tiles[j]
+                # Choice is to use tiles[i]
                 # Reflect current choice in state collection
                 perm.append(tiles[i])
                 used[i] = True
-                res.add(tuple(perm))
+                count += 1
                 # Recurse on next choice space of next state
                 backtrack()
                 # Clean up current choice (backtrack)
@@ -39,4 +42,4 @@ class Solution:
                 used[i] = False
 
         backtrack()
-        return len(res)
+        return count
