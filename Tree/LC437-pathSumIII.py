40bf73b7fd7d38ced367e70f155e3578a55c8520
Brandon Kenter <kenterbrandon@gmail.com>
2023-08-25 18:21:27 -0500

    updated approach to set prefixSums[0] to 1

--- Tree/LC437-pathSumIII.py
+++ Tree/LC437-pathSumIII.py
@@ -1,24 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix_map = defaultdict(int)
-        count = 0
+        prefixSums = defaultdict(int)
+        prefixSums[0] = 1
+        paths = 0
 
-        def dfs(cur, cur_sum):
-            nonlocal count
+        def preorder(cur, curSum):
+            nonlocal paths
             if cur is None:
                 return
-
-            cur_sum += cur.val
-            if cur_sum == targetSum:
-                count += 1
-
-            count += prefix_map[cur_sum - targetSum]
-            prefix_map[cur_sum] += 1
-
-            dfs(cur.left, cur_sum)
-            dfs(cur.right, cur_sum)
-
-            prefix_map[cur_sum] -= 1
-
-        dfs(root, 0)
-        return count
+            
+            curSum += cur.val
+            paths += prefixSums[curSum - targetSum]
+            prefixSums[curSum] += 1
+            preorder(cur.left, curSum)
+            preorder(cur.right, curSum)
+            prefixSums[curSum] -= 1
+        
+        preorder(root, 0)
+        return paths
+        
