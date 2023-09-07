0c5f2b93caeae425b14cac5855e23ff0fe63dbbc
Brandon Kenter <kenterbrandon@gmail.com>
2023-09-07 17:34:46 -0500

    Update LC1869-longerContiguousSegmentsOfOnesThanZeros.py

--- Sliding Window/LC1869-longerContiguousSegmentsOfOnesThanZeros.py
+++ Sliding Window/LC1869-longerContiguousSegmentsOfOnesThanZeros.py
@@ -4,12 +4,12 @@ class Solution:
         def find_longest(val):
             max_consec, left = 0, 0
             for right in range(len(s)):
-                if s[right] == val:
-                    max_consec = max(max_consec, right - left + 1)
-                else:
+                if s[right] != val:
                     left = right + 1
+                max_consec = max(max_consec, right - left + 1)
             return max_consec
         
         max_zero = find_longest('0')
         max_one = find_longest('1')
         return max_one > max_zero
+        
