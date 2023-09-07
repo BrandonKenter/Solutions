55cd1a3c0b0b430b994f906cde56dd7fb5920fc2
Brandon Kenter <kenterbrandon@gmail.com>
2023-09-07 16:29:23 -0500

    updated

--- Sliding Window/LC849-maximizeDistanceToClosestPerson.py
+++ Sliding Window/LC849-maximizeDistanceToClosestPerson.py
@@ -2,22 +2,17 @@ class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
         n = len(seats)
         maxi = left = 0
-        for right in range(1, len(seats)):
+        for right in range(len(seats)):
             # Case where we found a seated person
             if seats[right] == 1:
-                # There is a left seated person
-                # Take the middle position
+                m = (left + right) // 2
                 if seats[left] == 1:
-                    m = (left + right) // 2
                     maxi = max(maxi, m - left)
-                # There is not a left seated person
-                # Greedily take the leftmost position
                 else:
-                    maxi = max(maxi, right - left)
+                    maxi = max(maxi, right)
                 left = right
         # There is not a right seated person
         # Greedily take the rightmost position
         if seats[n-1] == 0:
             maxi = max(maxi, n - 1 - left)
         return maxi
-        
