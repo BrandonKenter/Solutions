f6341354ad5d2587b9ea5a08b501703e2154f07e
Brandon Kenter <kenterbrandon@gmail.com>
2023-09-19 15:39:26 -0500

    Update LC1375-numberOfTimesBinaryStringIsPrefix-Aligned.py

--- Array/LC1375-numberOfTimesBinaryStringIsPrefix-Aligned.py
+++ Array/LC1375-numberOfTimesBinaryStringIsPrefix-Aligned.py
@@ -1,15 +1,9 @@
 class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
-        total_zero = len(flips)
-        pre_i = count = 0
-        flipped = [0] * len(flips)
-        for flip_i in flips:
-            flip_i -= 1
-            flipped[flip_i] = 1
-            total_zero -= 1
-            while pre_i < len(flips) and flipped[pre_i] == 1:
-                pre_i += 1
-            if flip_i < pre_i and total_zero == len(flips) - pre_i:
-                count += 1
-        return count
-        
+        largest = flipCount = res = 0
+        for i in flips:
+            flipCount += 1
+            largest = max(largest, i)
+            if largest == flipCount:
+                res += 1
+        return res
